f89fc2a2ddbda77557dbcdd5c94a40aecbf9d3df
Kiylah O'Brien <kiylah2000@gmail.com>
2019-10-30 23:22:58 -0400

    added code to check if there is an ID # and if the quantity matches with what is in stock

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,66 +1,87 @@
 var mysql = require("mysql");
-var inquirer = require ("inquirer");
+var inquirer = require("inquirer");
 
 var connection = mysql.createConnection({
-  host: "localhost",
+    host: "localhost",
 
-  // Your port; if not 3306
-  port: 3306,
+    // Your port; if not 3306
+    port: 3306,
 
-  // Your username
-  user: "root",
+    // Your username
+    user: "root",
 
-  // Your password
-  password: "princessK95$",
-  database: "bamazon_db"
+    // Your password
+    password: "princessK95$",
+    database: "bamazon_db"
 });
 
-connection.connect(function(err) {
-  if (err) throw err;
-  console.log("connected as id " + connection.threadId);
-  afterConnection();
-});
-
-function afterConnection() {
-  connection.query("SELECT * FROM products", function(err, res) {
+connection.connect(function (err) {
     if (err) throw err;
-    console.log(res);
-    connection.end();
-  });
-}
-
-
-function Product (number,units) {
-    this.number = number;
-    this.units = units;  
-}
-
-Product.prototype.printInfo = function() {
-    console.log("ID #: " + this.number + "\nUnits: " + this.units);
-  };
-
-inquirer.prompt ([
-{
-    name: "Id #",
-    message: "What is the ID # of the prodcut that you are looking for (1-10)?"
-},
-{
-    name: "Units",
-    message: "How many units of this product would you like to buy?",
-},
-]).then((answer) =>{
-    var cart = new Cart(answer.number, answer.units);
-    cart.printStats();
+    console.log("connected as id " + connection.threadId);
+    readProducts();
 });
 
-
-// Write code here to parse command line arguments and store them into variables
-
-var idNumber = process.argv[2];
-var units = process.argv.splice(6).join(" ");
-
-// Add code to print whether there is enough inventory of the item that was chosen
-
-if(units < units) {
-    console.log("Insufficient quantity");
+// Function that accesses Mysql database and returns the item ID, department, price, product name
+function readProducts() {
+    console.log("Here is a list of all products that we offer....\n");
+    connection.query("SELECT * FROM products", function (err, res) {
+        if (err) throw err;
+        // Log all results of the SELECT statement
+        console.table(res);
+        Questions();
+        connection.end();
+    });
 }
+
+// Function that uses inquirer to promt the customer and asks what do the want to buy and how many units do they want
+function Questions() {
+
+    inquirer.prompt([
+        {
+            name: "purchaseID",
+            type: "input",
+            message: "What is the ID # of the prodcut that you are looking for (1-10)?",
+            validate: function(value) {
+                if (isNaN(value) === false) {
+                  return true;
+                }
+                return false;
+              }
+        },
+        {
+            name: "purchaseUnits",
+            type: "input",
+            message: "How many units of this product would you like to buy?",
+            validate: function(value) {
+                if (isNaN(value) === false) {
+                  return true;
+                }
+                return false;
+              }
+        },
+    ]).then((purchases) => {
+        // Check to see of the given item # exists with the desired quantity
+        connection.query("SELECT item_id, product_name, stock_quantity, price FROM products WHERE ?", { item_id: purchases.purchaseID }, 
+        function (err, data) {
+            if (err) throw err;
+            if (data[0].stock_quantity >= purchases.purchaseUnits) {
+                //updates the quantities in the database
+                var purchaseTotal = purchases.purchaseUnits * data[0].price;
+                var query = ("UPDATE products SET stock_quantity = " + (data[0].stock_quantity - purchases.purchaseUnits) + (purchaseTotal) + "WHERE item_id= "+ purchases.purchaseID);
+                connection.query(query, function(err, data) {
+                    if (err) throw err;
+                    console.log("Your total is: " + purchaseTotal);
+                    console.log("Congratulations, the product you requested is in stock! Placing Your order!");
+
+                });
+            }
+            else {
+                console.log("Sorry, there is not enough product in stock, your order can not be placed.");
+                console.log("Please modify your order.");
+                console.log("\n---------------------------------------------------------------------\n");
+                readProducts();
+            }
+        }
+
+    )}
+)};
